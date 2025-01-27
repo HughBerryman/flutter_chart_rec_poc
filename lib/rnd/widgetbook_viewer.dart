88af0af776c5ff7140903c18c3fd2db373fbae42
Hugh <hgberryman8@gmail.com>
2025-01-27 13:11:48 -0700

    adding widgets to widgetviewer

--- lib/rnd/widgetbook_viewer.dart
+++ lib/rnd/widgetbook_viewer.dart
@@ -7,18 +7,34 @@ import 'chart_app.dart';
 import 'rec_selector.dart';
 import 'feed_blend_calculator.dart';
 import 'welcome_page.dart';
+import 'feed_blend_calculator/components/right_panel.dart';
+import 'feed_blend_calculator/components/feed_app_bar.dart';
+import 'feed_blend_calculator/components/feed_parameters_section.dart';
+import 'feed_blend_calculator/components/feed_material_assays.dart';
+import 'feed_blend_calculator/models/lot.dart';
 
 class WidgetbookViewer extends StatelessWidget {
   const WidgetbookViewer({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Mock data for the widgets
+    final mockVoidCallback = () {};
+    final mockDoubleCallback = (double value) {};
+    final mockDateCallback = (DateTime? date) {};
+    final mockSetStringCallback = (Set<String> value) {};
+    final mockStringCallback = (String? value) {};
+    final mockBagCallback = (String id, int value) {};
+    final mockExpandCallback = (String id, bool value) {};
+    final mockAssayCallback = (String id, Map<String, double> value) {};
+    final mockBoolCallback = (bool value) {};
+
     return Widgetbook(
       initialRoute: 'welcome/welcome-page',
       directories: [
         WidgetbookComponent(
           name: 'Welcome',
-          isInitiallyExpanded: true, // false => collapses by default
+          isInitiallyExpanded: true,
           useCases: [
             WidgetbookUseCase(
               name: 'Welcome Page',
@@ -28,7 +44,7 @@ class WidgetbookViewer extends StatelessWidget {
         ),
         WidgetbookCategory(
           name: 'Feed Blend Calculator',
-          isInitiallyExpanded: false, // false => collapses by default
+          isInitiallyExpanded: false,
           children: [
             WidgetbookFolder(
               name: 'Screens',
@@ -46,13 +62,82 @@ class WidgetbookViewer extends StatelessWidget {
             ),
             WidgetbookFolder(
               name: 'Widgets',
-              children: [], // This will house the smaller widgets
+              children: [
+                WidgetbookComponent(
+                  name: 'Right Panel',
+                  useCases: [
+                    WidgetbookUseCase(
+                      name: 'Default',
+                      builder: (context) => RightPanel(
+                        lots: const [],
+                        width: 300,
+                        onWidthChanged: mockDoubleCallback,
+                        feedRate: 100,
+                        sieProduction: 200,
+                      ),
+                    ),
+                  ],
+                ),
+                WidgetbookComponent(
+                  name: 'Feed App Bar',
+                  useCases: [
+                    WidgetbookUseCase(
+                      name: 'Default',
+                      builder: (context) => FeedAppBar(
+                        selectedLotCount: 0,
+                        totalLotCount: 10,
+                        onViewSaved: mockVoidCallback,
+                        onSaveStrategy: mockVoidCallback,
+                        onExport: mockVoidCallback,
+                        isPanelVisible: true,
+                        onPanelToggle: mockVoidCallback,
+                      ),
+                    ),
+                  ],
+                ),
+                WidgetbookComponent(
+                  name: 'Feed Parameters',
+                  useCases: [
+                    WidgetbookUseCase(
+                      name: 'Default',
+                      builder: (context) => FeedParametersSection(
+                        feedRate: 100,
+                        sieProduction: 200,
+                        onFeedRateChanged: mockDoubleCallback,
+                        onSieProductionChanged: mockDoubleCallback,
+                        onStartDateChanged: mockDateCallback,
+                        onTargetEndDateChanged: mockDateCallback,
+                      ),
+                    ),
+                  ],
+                ),
+                WidgetbookComponent(
+                  name: 'Material Assays',
+                  useCases: [
+                    WidgetbookUseCase(
+                      name: 'Default',
+                      builder: (context) => FeedMaterialAssays(
+                        lots: const [],
+                        showSelected: true,
+                        selectedLocations: <String>{},
+                        selectedSort: null,
+                        onShowSelectedChanged: mockBoolCallback,
+                        onLocationsChanged: mockSetStringCallback,
+                        onSortChanged: mockStringCallback,
+                        onBagsChanged: mockBagCallback,
+                        onExpandChanged: mockExpandCallback,
+                        onAssayValuesChanged: mockAssayCallback,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
         WidgetbookCategory(
           name: 'Components',
-          isInitiallyExpanded: false, // false => collapses by default
+          isInitiallyExpanded: false,
           children: [
             WidgetbookComponent(
               name: 'Chart App',
