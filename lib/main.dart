387213888e292c4b7874790743bdacde0615de18
Hugh <hgberryman8@gmail.com>
2025-01-26 18:12:32 -0700

    refactor(main): remove app bar and simplify theme configuration

--- lib/main.dart
+++ lib/main.dart
@@ -34,68 +34,7 @@ class MyApp extends StatelessWidget {
             surfaceTint: FMIThemeBase.basePaletteCoolGrayCoolGray100,
           ),
           scaffoldBackgroundColor: FMIThemeBase.basePaletteCoolGrayCoolGray95,
-          textTheme: TextTheme(
-            displayLarge: TextStyle(
-              fontSize: FMIThemeBase.baseDisplayLargeFontSize,
-              fontFamily: FMIThemeBase.baseDisplayLargeFontFamily,
-              fontWeight: FMIThemeBase.baseDisplayLargeFontWeight,
-            ),
-            displayMedium: TextStyle(
-              fontSize: FMIThemeBase.baseDisplayMediumFontSize,
-              fontFamily: FMIThemeBase.baseDisplayMediumFontFamily,
-              fontWeight: FMIThemeBase.baseDisplayMediumFontWeight,
-            ),
-            displaySmall: TextStyle(
-              fontSize: FMIThemeBase.baseDisplaySmallFontSize,
-              fontFamily: FMIThemeBase.baseDisplaySmallFontFamily,
-              fontWeight: FMIThemeBase.baseDisplaySmallFontWeight,
-            ),
-            headlineLarge: TextStyle(
-              fontSize: FMIThemeBase.baseHeadlineLargeFontSize,
-              fontFamily: FMIThemeBase.baseHeadlineLargeFontFamily,
-              fontWeight: FMIThemeBase.baseHeadlineLargeFontWeight,
-            ),
-            headlineMedium: TextStyle(
-              fontSize: FMIThemeBase.baseHeadlineMediumFontSize,
-              fontFamily: FMIThemeBase.baseHeadlineMediumFontFamily,
-              fontWeight: FMIThemeBase.baseHeadlineMediumFontWeight,
-            ),
-            headlineSmall: TextStyle(
-              fontSize: FMIThemeBase.baseHeadlineSmallFontSize,
-              fontFamily: FMIThemeBase.baseHeadlineSmallFontFamily,
-              fontWeight: FMIThemeBase.baseHeadlineSmallFontWeight,
-            ),
-            titleLarge: TextStyle(
-              fontSize: FMIThemeBase.baseTitleLargeFontSize,
-              fontFamily: FMIThemeBase.baseTitleLargeFontFamily,
-              fontWeight: FMIThemeBase.baseTitleLargeFontWeight,
-            ),
-            titleMedium: TextStyle(
-              fontSize: FMIThemeBase.baseTitleMediumFontSize,
-              fontFamily: FMIThemeBase.baseTitleMediumFontFamily,
-              fontWeight: FMIThemeBase.baseTitleMediumFontWeight,
-            ),
-            titleSmall: TextStyle(
-              fontSize: FMIThemeBase.baseTitleSmallFontSize,
-              fontFamily: FMIThemeBase.baseTitleSmallFontFamily,
-              fontWeight: FMIThemeBase.baseTitleSmallFontWeight,
-            ),
-            bodyLarge: TextStyle(
-              fontSize: FMIThemeBase.baseBodyLargeFontSize,
-              fontFamily: FMIThemeBase.baseBodyLargeFontFamily,
-              fontWeight: FMIThemeBase.baseBodyLargeFontWeight,
-            ),
-            bodyMedium: TextStyle(
-              fontSize: FMIThemeBase.baseBodyMediumFontSize,
-              fontFamily: FMIThemeBase.baseBodyMediumFontFamily,
-              fontWeight: FMIThemeBase.baseBodyMediumFontWeight,
-            ),
-            bodySmall: TextStyle(
-              fontSize: FMIThemeBase.baseBodySmallFontSize,
-              fontFamily: FMIThemeBase.baseBodySmallFontFamily,
-              fontWeight: FMIThemeBase.baseBodySmallFontWeight,
-            ),
-          ),
+          textTheme: FmiThemeBase.fmiThemeDataBase.textTheme,
         ),
         home: const HomeScreen(),
       ),
@@ -117,14 +56,6 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: FmiAppBar(
-        title: 'Concept Ideation',
-        themeStyle: ThemeStyle.primary,
-        showInternet: false,
-        showSync: false,
-        onBackPressed: null,
-        closeIconAppBar: null,
-      ),
       body: Center(
         child: SingleChildScrollView(
           child: ConstrainedBox(
